711617dd4ec7ce50dd4ce56129e325d8daa94711
zacksupreme <zacksupreme@gmail.com>
2015-04-27 17:14:41 +0700

    Merge origin/master
    Conflicts:
    	application/views/transaksi/perjalanandinas/edit.php

---
+++ application/views/admin/report/bukti_perjalanan_dinas/report_daftar_pengeluaran_riil.php
@@ -0,0 +1,84 @@
+<html>
+    <body>
+    <center>
+        <img height="20%" width="100%" src="<?php echo $_SERVER['DOCUMENT_ROOT'] . '/esatker/assets/img/' ?>header surat.jpg" />
+    </center>
+    <center>
+        <h3>DAFTAR PENGELUARAN RIIL</h3>
+        <p>&nbsp;</p>
+    </center>
+    <p>Yang bertanda tangan di bawah ini :
+      <label><br>
+      </label>
+    </p>
+    <table style="width:100%">
+      <tr>
+        <td width="21%"><div align="left">Nama</div></td>
+        <td width="2%"><div align="center">:</div></td>
+        <td width="77%"><div align="left"></div></td>
+      </tr>
+      <tr>
+        <td>NIP</td>
+        <td><div align="center">:</div></td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td>Jabatan</td>
+        <td><div align="center">:</div></td>
+        <td>&nbsp;</td>
+      </tr>
+    </table>
+    <p>berdasarkan Surat Perintah Perjalanan Dinas (SPPD) Nomor ................................. tanggal, ............................dengan ini kami menyatakan dengan sesungguhnya bahwa :</p>
+    <p>1. Biaya transport pegawai dan/atau biaya penginapan di bawah ini yang tidak dapat diperoleh bukti-bukti pengeluarannya, meliputi :</p>
+    <table style="width:100%" border="1">
+        <tr>
+            <td width="21%"><div align="center">No</div></td>
+            <td width="54%"><div align="center">Uraian</div></td>
+            <td width="25%"><div align="center">Jumlah (Rp.)</div></td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td><div align="center">Jumlah (Rp)</div></td>
+            <td>&nbsp;</td>
+        </tr>
+    </table>
+    
+    <p>2. Jumlah urang tersebut pada angka 1 di atas benar-benar dikeluarkan untuk pelaksanaan perjalanan dinas dimaksud dan apabila di kemudian hari terdapat kelebihan atas pembayaran, kami bersedia untuk menyetorkan kelebihan tersebut ke Kas Negara.</p>
+    <p>&nbsp;</p>
+    <p>Demikian pernyataan ini kami buat dengan sebenarnya, untuk dipergunakan sebagaimana mestinya.<br>
+      
+    </p>
+     <table style="width: 100%">
+        <tr>
+            <td align="center" width="50%"><br>
+                <br>
+              <p>Mengetahui/menyetujui<br>
+              An. Kepala Satuan Kerja<br>
+              Pejabat Pembuat Komitmen,</p>
+
+              <p>&nbsp;</p>
+                <p><br>
+                  Iwan Suprijanto, ST, MT<br>
+NIP: 197109301998031001 </p>
+                <br>
+                <br>
+
+            </td>
+            <td align="center" width="50%"><p>Bandung, .........<br>
+                Yang melakukan perjalanan,<br>
+                <br>
+                </p>
+              <p>&nbsp;</p>
+              <p>...................................</p>
+              <p>NIP.<br>
+                <br>
+            </p></td>
+        </tr>
+    </table>
+</body>
+</html>
---
+++ application/views/admin/report/daftar_biaya_perjalanan/report_daftar_nominatif_perjalanan_dinas.php
@@ -0,0 +1,110 @@
+<html>
+    <body>
+    <center>
+        <img height="20%" width="100%" src="<?php echo $_SERVER['DOCUMENT_ROOT'] . '/esatker/assets/img/' ?>header surat.jpg" />
+    </center>
+    <center>
+        <h3><u>DAFTAR NOMINATIF PERJALANAN DINAS</u></h3>
+      <p>Nomor : 82492349287307347297</p>
+        <p>
+          <label><br>
+          </label>
+        </p>
+    </center>
+    <table style="width:100%">
+      <tr>
+        <td width="21%"><div align="left">1. Kode Kantor/Satker</div></td>
+        <td width="2%"><div align="center">:</div></td>
+        <td width="77%"><div align="left"></div></td>
+      </tr>
+      <tr>
+        <td>2. Nama Kantor/Satker</td>
+        <td><div align="center">:</div></td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td>3. Tanggal dan Nomor DIPA</td>
+        <td><div align="center">:</div></td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td>4. Jenis Belanja</td>
+        <td><div align="center">:</div></td>
+        <td>&nbsp;</td>
+      </tr>
+    </table>
+    <p>&nbsp;</p>
+    <table style="width:100%" border="1">
+       <thead>
+                <tr>
+                    <th rowspan="2" width="3%">No</th>
+                    <th rowspan="2" width="15%">Nama</th>
+                    <th rowspan="2" width="8%">Gol</th>
+                    <th rowspan="2" width="15%">Jabatan</th>
+                    <th rowspan="2" width="8%">Tujuan</th>
+                    <th colspan="2">Tanggal</th>
+                    <th rowspan="2" width="8%">Lama Perj. Dinas</th>
+                    <th colspan="5">Rincian Kebutuhan Dana (Rp)</th>
+                    <th rowspan="2"  width="3%" class="td-actions">Keterangan</th>
+                </tr>
+                <tr>
+                    <th width="8%">Berangkat</th>
+                    <th width="8%">Kembali</th>
+                    
+                    <th width="6%">Uang Harian</th>
+                    <th width="6%">Uang Ref</th>
+                    <th width="6%">Trans</th>
+                    <th width="6%">Penginapan</th>
+                    <th width="6%">Jumlah</th>
+                </tr>
+            </thead>
+       <tbody>
+               
+                    <tr>
+                        <td>10</td>
+                        <td>Taufik Ismail A</td>
+                        <td>III/A</td>
+                        <td>Juru Ketik</td>
+                        <td>Jakarta</td>
+                        <td>22-04-2015</td>
+                        <td>23-04-2015</td>
+                        <td>&nbsp;</td>
+                        <td><?php echo number_format(2000000) ?></td>
+                        <td>-</td>
+                        <td><?php echo number_format(2000000) ?></td>
+                        <td><?php echo number_format(2000000) ?></td>
+                        <td><?php echo number_format(6000000) ?></td>
+                        <td>
+                           
+                        </td>
+                    </tr>
+                    </tbody>
+    </table>
+    
+    <p>&nbsp;</p>
+     <table style="width: 100%">
+        <tr>
+            <td align="center" width="50%"><br>
+              <p>KPPN Bandung II<br>
+              Kepala Seksi Pencairan Dana</p>
+
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p><br>
+                  ......................................<br>
+                <br>
+
+            </p></td>
+            <td align="center" width="50%"><p>Bandung, .........<br>
+                PEJABAT PEMBUAT KOMITMEN<br>
+                <br>
+                </p>
+              <p>&nbsp;</p>
+              <p>Iwan Suprijanto, ST, MT<br>
+NIP: 197109301998031001 <br>
+                <br>
+            </p></td>
+        </tr>
+    </table>
+</body>
+</html>
