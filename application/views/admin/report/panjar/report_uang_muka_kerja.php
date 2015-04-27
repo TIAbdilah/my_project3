34a85d0989fbee5996295fdf29a97c3fd2281dba
zacksupreme <zacksupreme@gmail.com>
2015-04-27 17:49:18 +0700

    Merge origin/master
    Conflicts:
    	application/views/transaksi/perjalanandinas/edit.php

---
+++ application/views/admin/report/panjar/report_uang_muka_kerja.php
@@ -0,0 +1,75 @@
+<html>
+    <body>
+    <center>
+        <img height="20%" width="100%" src="<?php echo $_SERVER['DOCUMENT_ROOT'] . '/esatker/assets/img/' ?>header surat.jpg" />
+    </center>
+    <center>
+        <h3><u>UANG MUKA KERJA</u></h3>
+        <p>Panjar Kerja</p>
+        <p>&nbsp;</p>
+        <label><br>
+        </label>
+    </center>
+    <table style="width:100%">
+      <tr>
+        <td width="21%"><div align="left">Sudah terima dari</div></td>
+        <td width="2%"><div align="center">:</div></td>
+        <td width="77%"><div align="left">Kepala Satuan Kerja Pusat Penelitian dan Pengembangan Permukiman</div></td>
+      </tr>
+      <tr>
+        <td>Banyaknya uang (rupiah)</td>
+        <td><div align="center">:</div></td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td>Untuk pembayaran</td>
+        <td><div align="center">:</div></td>
+        <td><p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p></td>
+      </tr>
+    </table>
+    <p><u>Jumlah Rp.</u></p>
+    <p>&nbsp;</p>
+    <p align="right">Bandung, </p>
+     <table style="width: 100%">
+        <tr>
+            <td align="center" width="50%"><br>
+                <br>
+              <p>Mengetahui/menyetujui<br>
+                <strong>An. Kepala Satuan Kerja</strong><br>
+              Pejabat Pembuat Komitmen,</p>
+
+              <p>&nbsp;</p>
+                <p><br>
+                  <strong><u>Iwan Suprijanto, ST, MT</u></strong><br>
+NIP: 197109301998031001 </p>
+                <br>
+                <br>
+
+            </td>
+            <td align="center" width="50%"><p>Setuju Bayar<br>
+                Bendahara Pengeluaran,<br>
+                </p>
+              <p>&nbsp;</p>
+              <p><strong><u>Drajat Subuhri</u></strong><br>
+              NIP. 1983200423042023201<br>
+                <br>
+            </p></td>
+            <td align="center" width="50%"><p>Yang menerima,</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>..........................................</p></td>
+        </tr>
+    </table>
+     <p>CATATAN :<br>
+     </p>
+     <ol>
+       <li>Uang muka/panjar ini harap dipertanggung jawabkan dengan Kwitansi dalam wktu 3 (tiga) hari kerja setelah uang muka dierima.</li>
+       <li>Uang muka/panjar untuk pengadaan/pembelian barang mulai dari Rp. 250.000,- pertanggung jawaban (kwitansi) harus dari pengusaha/rekanan yang mempunyai NPWP/faktur pajak dilengkapi dengan materai tempel Rp. 3.000,- dan untuk pengadaan/pembelian barang dengan nilai Rp. 1.000.000,- ke atas dilengkapi dengan meterai Rp. 6.000,-</li>
+       <li>Uang muka/panjar untuk pengadaan/pembelian barang dengan nilai Rp. 1.000.000,- ke atas dikenakan pajak sesuai dengan ketentuan yang berlaku.</li>
+       <li>Tidak diperkenankan meminta uang muka/panjar kembali apabila uang muka terdahulu belum dipertanggung-jawabkan.</li>
+       <li>Bukti kwitansi dan nota/faktur harus mencantumkan dengan jelas : Cap Perusahaan, Tanda Tangan dan Nama Jelas Pengusahanya.</li>
+     </ol>
+</body>
+</html>
