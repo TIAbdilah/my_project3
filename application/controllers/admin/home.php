cfcaf6a0c00188c1127c92a853333b869f2e4236
zacksupreme <zacksupreme@gmail.com>
2015-05-25 10:36:22 +0700

    fix id bug

--- application/controllers/admin/home.php
+++ application/controllers/admin/home.php
@@ -18,17 +18,19 @@ class Home extends CI_Controller {
         $this->load->model('master/users_model');
         $this->load->model('transaksi/perjalanan_dinas_model');
         $this->load->model('transaksi/pengajuan_barang_model');
+        $this->load->model('transaksi/pengajuan_honorarium_model');
         $this->is_logged_in();
     }
 
     public function index() {
         $data['title'] = "e-satker | Home";
         $role = $this->session->userdata('role');
-        
-        $array_custom = new Array_custom();        
+
+        $array_custom = new Array_custom();
         $data['list_data'] = $this->perjalanan_dinas_model->select_by_field(array('status' => $array_custom->int_role[$role]))->result();
         $data['list_data_barang'] = $this->pengajuan_barang_model->select_by_field(array('status_approval' => $array_custom->int_role[$role]))->result();
-        $data['page'] = 'admin/master/tasklist/list';        
+        $data['list_data_honorarium'] = $this->pengajuan_honorarium_model->select_by_field(array('status_approval' => $array_custom->int_role[$role]))->result();
+        $data['page'] = 'admin/master/tasklist/list';
         $data['status'] = $array_custom->status;
         $data['status_penolakan'] = $array_custom->status_penolakan;
         $data['status_approval'] = $array_custom->status;
